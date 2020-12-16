906a584a04d214ab8aea5b2ff9c8a1459dafbf94
chuhw3 <2664822644@qq.com>
2020-12-16 21:55:21 +0800

    v1.0.2

--- dal/db/sampleBlog/WriteBlog.go
+++ dal/db/sampleBlog/WriteBlog.go
@@ -4,12 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-
 	"github.com/chuhongwei/BlogServer/dal/db"
-<<<<<<< HEAD
-=======
-
->>>>>>> 122f99ecae4f8e51d7daa1e7adc2f9ce6340342b
 	"github.com/chuhongwei/BlogServer/dal/model"
 )
 
@@ -17,8 +12,7 @@ func main() {
 	articles := make([]model.Article, 7)
 	users := make([]model.User, 7)
 	titles := []string{"2021年GO语言（Golang就业班全系列", "Beego_ubuntu下golang及beego环境的全局配置", "Go学习实践", 
-					   "阿里巴巴十年Java架构师分享", "领域驱动实践总结",
-					   "BS架构及其运行原理", "Go&&阿里云服务器" }
+					   "阿里巴巴十年Java架构师分享", "领域驱动实践总结","BS架构及其运行原理", "Go&&阿里云服务器" }
 	authors := []string{"小龙in武汉", "Grayan", "Hi ning先森", "JAVA高级架构v", "张彦峰ZYF", "蜘蛛侠不会飞", "李歘歘"}
 	times := []string{"2020-09-17 14:04:21", "2019-03-13 22:28:32", "2019-11-04 11:43:11", 
 	                  "2019-05-14 17:38:32", "2020-03-26 18:34:46", "2018-03-23 22:31:19", "2019-07-19 14:23:37"}
